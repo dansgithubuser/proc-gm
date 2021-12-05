208fca1e61ba50df8339c86403426731b4ea9161
Dan <dansonlinepresence@gmail.com>
2021-12-04 21:08:00 -0500

    hold down key

--- proc_gm/__init__.py
+++ proc_gm/__init__.py
@@ -9,24 +9,30 @@ def run(w=640, h=480, fps=60):
     screen = pygame.display.set_mode((w, h))
     clock = pygame.time.Clock()
     world = World(tileset.VillageSerene())
+    key_down = None
     while True:
-        player_action = None
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                player_action = {
-                    pygame.K_w: 'n',
-                    pygame.K_e: 'ne',
-                    pygame.K_d: 'e',
-                    pygame.K_c: 'se',
-                    pygame.K_x: 's',
-                    pygame.K_z: 'sw',
-                    pygame.K_a: 'w',
-                    pygame.K_q: 'nw',
-                }.get(event.key)
+                key_down = event.key
+                continue
+            if event.type == pygame.KEYUP:
+                if key_down == event.key:
+                    key_down = None
                 continue
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        player_action = {
+            pygame.K_w: 'n',
+            pygame.K_e: 'ne',
+            pygame.K_d: 'e',
+            pygame.K_c: 'se',
+            pygame.K_s: 's',
+            pygame.K_x: 's',
+            pygame.K_z: 'sw',
+            pygame.K_a: 'w',
+            pygame.K_q: 'nw',
+        }.get(key_down)
         world.update(player_action)
         screen.fill((0, 0, 0))
         world.render(screen)
